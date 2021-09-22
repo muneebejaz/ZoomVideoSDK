6aa0967db18640cbae41dee0d146fe5149a504f5
Muhammad Muneeb Ajaz <muneeb.ejaz@outlook.com>
2021-09-22 11:00:38 +0500

    first commit

---
+++ config/dev.ts
@@ -0,0 +1,10 @@
+import { getExploreName } from '../utils/platform';
+
+export const devConfig = {
+  sdkKey: '74SZmtXzdxzIHrf6zzcZOgZlJgE9SzfM72sF',
+  sdkSecret: 'PnClsA0iWtqYW1x4SgjiHceUbAK9cAxrnFJX',
+  // topic: '322',
+  name: getExploreName(),
+  password: '',
+  // signature: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiNzRTWm10WHpkeHpJSHJmNnp6Y1pPZ1psSmdFOVN6Zk03MnNGIiwiaWF0IjoxNjMyMDQ5NjU2LCJleHAiOjE2MzIwNTY4NTYsInRwYyI6IjMyMiIsInB3ZCI6IiJ9.uYY1XuUJ6UZ8jEdt34l-dkXtEREUCwWELaQ63H_Zc7E',
+};
